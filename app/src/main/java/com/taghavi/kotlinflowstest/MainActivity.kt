f8935736156efe4672023950b54731cc33c5d5da
EhsanTgv <EhsanTgv@gmail.com>
2022-01-04 15:50:55 +0300

    Initial commit

---
+++ app/src/main/java/com/taghavi/kotlinflowstest/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.taghavi.kotlinflowstest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.taghavi.kotlinflowstest.ui.theme.KotlinFlowsTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinFlowsTestTheme {
+
+            }
+        }
+    }
+}
